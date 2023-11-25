e959393446e6567794b16e4f281070558821aae8
Subhash Reddy <subhashreddy22@gmail.com>
2023-11-25 21:45:41 +0100

    add test

--- src/com/leetcode/string/LongestSubstringWithoutRepetition.java
+++ src/com/leetcode/string/LongestSubstringWithoutRepetition.java
@@ -27,8 +27,8 @@ public class LongestSubstringWithoutRepetition {
         int max = 0;
 
         for (int i = 0, j = 0; i < s.length(); i++){
-            if (map.containsKey(s.charAt(i))){
-                j = Math.max(j,map.get(s.charAt(i)) + 1);
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
             }
             map.put(s.charAt(i), i);
             max = Math.max(max, i - j + 1);

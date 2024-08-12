8b92281e366dd13680ca0d710b92618bcdef9baa
Subhash Reddy <subhashreddy22@gmail.com>
2024-08-12 11:22:47 +0200

    add GroupAnagrams

---
+++ src/com/leetcode/array/GroupAnagrams.java
@@ -0,0 +1,45 @@
+package com.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// https://leetcode.com/problems/group-anagrams/
+public class GroupAnagrams {
+
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[] {"eat","tea","tan","ate","nat","bat"}));
+        System.out.println(groupAnagrams(new String[] {""}));
+        System.out.println(groupAnagrams(new String[] {"a"}));
+    }
+
+    private static List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> result = new HashMap<>();
+
+        for (String str : strs) {
+            int[] count = new int[26];
+
+            for (char ch : str.toCharArray()) {
+                count[ch - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 26; i++) {
+                sb.append(count[i]);
+            }
+
+            String key = sb.toString();
+
+            if (!result.containsKey(key)) {
+                result.put(key, new ArrayList<>());
+            }
+
+            result.get(key).add(str);
+        }
+
+        return new ArrayList<>(result.values());
+    }
+}
